88e774b8b3dde5a20785dd72839b941725eeac86
Aman Bagri <amanbagri95@gmail.com>
2023-09-20 16:09:36 +0530

    igonore eslint during build

--- next.config.js
+++ next.config.js
@@ -2,14 +2,20 @@
 const nextConfig = {
     async redirects() {
         return [
-          {
-            source: '/(.*)#$',
-            destination: '/about',
-            permanent: true,
-          },
+            {
+                source: '/(.*)#$',
+                destination: '/about',
+                permanent: true,
+            },
         ]
-      },
-    
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+
+
 }
 
 module.exports = nextConfig
